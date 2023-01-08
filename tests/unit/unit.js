e05011477748412b06cd1a67c2841e8f8e1d065e
Ana Díaz Muñoz <58432799+aniitadiazm@users.noreply.github.com>
2023-01-08 18:09:46 +0100

    unit tests

--- tests/unit/unit.js
+++ tests/unit/unit.js
@@ -6,7 +6,7 @@ chai.expect();
 chai.should();
 
 let recipesBookId;
-let recipesBookPOST = { name: "test", summary: "test", recipeList: "test", idUser: "test" }
+let recipesBookPOST = { name: "test_POST", summary: "test_POST", recipeList: "test_POST", idUser: "test_POST" }
 let recipesBookPUT = { name: "test_PUT", summary: "test_PUT", recipeList: "test_PUT", idUser: "test_PUT" }
 
 const apiURL = "http://localhost:8080"
@@ -25,7 +25,8 @@ describe('get Recipes Books', () => {
 describe('post Recipes Books', () => {
     it('should post an recipes book', () => {
         chai.request(apiURL)
-        .get('/api/v1/recipesbooks')
+        .post('/api/v1/recipesbooks')
+        .send(recipesBookPOST)
         .end((err, res) => {
             res.body.should.be.a('object');
             res.body.should.have.property('name');
@@ -38,16 +39,42 @@ describe('post Recipes Books', () => {
     })
 })
 
-describe('get id of Recipes Book', () => {
-    it('get id of an recipes book', () => {
+describe('get id Recipes Book', () => {
+    it('should get id of an recipes book', () => {
         chai.request(apiURL)
-        .get('/api/v1/recipesbooks')
+        .get('/api/v1/recipesbooks' + recipesBookId)
         .end((err, res) => {
             res.body.should.be.a('object');
             res.body.should.have.property('name').eql(recipesBookPOST.name);
             res.body.should.have.property('summary').eql(recipesBookPOST.summary);
             res.body.should.have.property('recipeList').eql(recipesBookPOST.recipeList);
             res.body.should.have.property('idUser').eql(recipesBookPOST.idUser);
+            res.body.should.have.property('_id').eql(recipesBookId);
+        })
+    })
+})
+
+describe('put Recipes Book', () => {
+    it('should update recipes book', () => {
+        chai.request(apiURL)
+        .put('/api/v1/recipesbooks' + recipesBookId)
+        .send(recipesBookPUT)
+        .end((err, res) => {
+            res.body.should.be.a('object');
+            res.body.should.have.property('name').eql(recipesBookPUT.name);
+            res.body.should.have.property('summary').eql(recipesBookPUT.summary);
+            res.body.should.have.property('recipeList').eql(recipesBookPUT.recipeList);
+            res.body.should.have.property('idUser').eql(recipesBookPUT.idUser);
+        })
+    })
+})
+
+describe('delete Recipes Book', () => {
+    it('should delete recipes book', () => {
+        chai.request(apiURL)
+        .delete('/api/v1/recipesbooks' + recipesBookId)
+        .end((err, res) => {
+            res.should.have.status(204);
         })
     })
 })
