838ae253a25aaf098885a41f5e0dc86a5ceb7dda
Ana Díaz Muñoz <58432799+aniitadiazm@users.noreply.github.com>
2023-01-10 11:31:07 +0100

    services

--- services/recipesBookManager.js
+++ services/recipesBookManager.js
@@ -3,12 +3,11 @@ import {logger} from "@oas-tools/commons";
 import { Types } from 'mongoose';
 import { CircuitBreaker } from "../circuitBreaker/circuitBreaker.js";
 
-
 export async function findByRecipesBookId(req, res) {
 
-  const idRecipesBook = req.params._id;
+  const idRecipesBook = req.params.id;
   
-    CircuitBreaker.getBreaker(RecipesBook).fire("findById", {_id: idRecipesBook}).then(result => {
+    CircuitBreaker.getBreaker(RecipesBook).fire("find", {_id: idRecipesBook}).then(result => {
 
       if (result) {
         res.send(result);
@@ -16,15 +15,16 @@ export async function findByRecipesBookId(req, res) {
       } else {
         res.sendStatus(404);
       }
-    }).catch ((e) => {
-      res.sendStatus(500);
+      
+    }).catch ((err) => {
+      res.status(500).send({ error: err.message });
     });
 }
 
 export async function updateRecipesBook(req, res) {
 
     const body = req.body;
-    const idRecipesBook = req.params._id;
+    const idRecipesBook = req.params.id;
     
         CircuitBreaker.getBreaker(RecipesBook).fire("findByIdAndUpdate", {_id: idRecipesBook}, body).then(result => {
           if(result) {
@@ -32,53 +32,51 @@ export async function updateRecipesBook(req, res) {
           } else {
             res.sendStatus(404);
           }
-        }).catch ((e) => {
-          res.sendStatus(500);
+        }).catch ((err) => {
+          res.status(500).send({ error: err.message });
         })
 }
 
 export async function deleteRecipesBook(req, res) {
     
-  const idRecipesBook = req.params._id;
+  const idRecipesBook = req.params.id;
 
     try {
       CircuitBreaker.getBreaker(RecipesBook).fire("delete", {_id: idRecipesBook});
       return res.sendStatus(201);
 
-      } catch (e) {
-        res.status(400).send({ error: e.message });
+      } catch (err) {
+        res.status(400).send({ error: err.message });
       }
 }
 
 
 export async function getRecipesBook(req, res) {
 
-  CircuitBreaker.getBreaker(RecipesBook).fire("find", {}).then((results) => {
-    res.send(results);
+  CircuitBreaker.getBreaker(RecipesBook).fire("find", {}).then((result) => {
+    res.send(result);
     
   }).catch((err) => {
-    res.sendStatus(500);
+    res.status(500).send({ error: err.message });
   })
 }
 
 export async function findByUserId(req, res) {
 
-    const username = req.params.idUser;
+  const username = req.params.idUser;
 
-      try {
-        CircuitBreaker.getBreaker(RecipesBook).fire("find", {idUser: username}).then(result => {
+  CircuitBreaker.getBreaker(RecipesBook).fire("find", {idUser: username}).then(result => {
 
-          if (result) {
-            res.send(result);
+    if (result) {
+      res.send(result);
 
-          } else {
-            res.status(404);
-          }
-        })
+    } else {
+      res.status(404);
+    }
 
-      } catch (e) {
-          res.sendStatus(501);
-        }
+  }).catch ((err) => {
+          res.status(501).send({ error: err.message });
+  })
 }
 
 export async function addRecipesBook(req, res) {
@@ -88,7 +86,7 @@ export async function addRecipesBook(req, res) {
       try {
         CircuitBreaker.getBreaker(RecipesBook).fire("create", newRecipesBook);
         return res.sendStatus(newRecipesBook);
-      } catch (e) {
-        res.status(400).send({ error: e.message });
+      } catch (err) {
+        res.status(400).send({ error: err.message });
       }
 }
